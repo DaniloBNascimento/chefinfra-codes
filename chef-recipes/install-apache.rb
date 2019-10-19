657af535bed3c6025fde8230493b4d1e30d106bb
Danilo Bastos <danilobastos2@gmail.com>
2019-10-19 21:41:14 +0000

    Intall Apache Server in Ubuntu

--- chef-recipes/install-apache.rb
+++ chef-recipes/install-apache.rb
@@ -1,11 +1,20 @@
+# Receita para instalação, inicialização e ativação do Apache Server
+# Instalação utilizando o resource apt em um servidor Ubuntu
+
+# Atualizando repo de pacotes
+
 apt_update 'update' do
   action :update
 end
+
+# Instalando o apache 
 
 apt_package 'apache2' do
   action :install
 
 end
+
+# Habilitando o serviço na inicialização e "estartando" o serviço
 
 service 'apache2' do
   supports :status => true, :restart => true, :reload => true
